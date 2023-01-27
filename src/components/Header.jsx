971092d29a4462c3cd44bea3ac1d64a7ed03a109
RafathAmaan Mohammad <116374643+rafathamaan@users.noreply.github.com>
2023-01-28 01:39:56 +0530

    f

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
 
-import Logo from "../img/logo.png";
+import Logo from "/Users/rafathamaan/Documents/GitHub/foodapp-yt-mar22/src/img/mlritlogo.png";
 import Avatar from "../img/avatar.png";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../context/StateProvider";
@@ -52,12 +52,12 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
+    <header className="fixed z-20 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
-          <img src={Logo} className="w-8 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold"> City</p>
+          <img src={Logo} className="w-40 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold"> Digital Canteen</p>
         </Link>
 
         <div className="flex items-center gap-8">
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -12,7 +12,7 @@ const HomeContainer = () => {
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
-            Bike Delivery
+            Self Pickup
           </p>
           <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
             <img
@@ -24,17 +24,14 @@ const HomeContainer = () => {
         </div>
 
         <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
-          The Fastest Delivery in
+         Welcome to {"\n"}
           <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
-            Your City
+              Digital Canteen
           </span>
         </p>
 
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
-          eaque fugit distinctio est nam voluptatum architecto, porro iusto
-          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
-          suscipit!
+          Order from here and pickup from the canteen as simple as that
         </p>
 
         <button
